ec7505c1d037765d83973a8e8cf088332040888c
MutsumiMatsuda <matsuda_mutsumi@r3.dion.ne.jp>
2021-11-21 23:35:31 +0900

    sql14

---
+++ database/sql/dump07.sql
@@ -0,0 +1,18 @@
+INSERT INTO howtos (id, name, created_at, updated_at) VALUES (1,'圧力鍋','2021-11-01 20:26:42','2021-11-10 18:17:30');
+INSERT INTO howtos (id, name, created_at, updated_at) VALUES (2,'オーブン','2021-11-01 20:26:42','2021-11-01 20:26:42');
+INSERT INTO howtos (id, name, created_at, updated_at) VALUES (3,'レンジ','2021-11-01 20:26:42','2021-11-01 20:26:42');
+INSERT INTO howtos (id, name, created_at, updated_at) VALUES (4,'蒸し器','2021-11-10 18:17:59','2021-11-10 18:18:15');
+
+INSERT INTO material_categories (id, name, created_at, updated_at) VALUES (1,'豆類','2021-11-03 02:04:59','2021-11-03 02:04:59');
+INSERT INTO material_categories (id, name, created_at, updated_at) VALUES (2,'穀類','2021-11-03 02:04:59','2021-11-03 02:04:59');
+INSERT INTO material_categories (id, name, created_at, updated_at) VALUES (3,'野菜','2021-11-03 02:04:59','2021-11-03 02:04:59');
+INSERT INTO material_categories (id, name, created_at, updated_at) VALUES (4,'果実・香辛料類','2021-11-03 02:04:59','2021-11-03 02:04:59');
+INSERT INTO material_categories (id, name, created_at, updated_at) VALUES (5,'きのこ・海藻','2021-11-03 02:04:59','2021-11-03 02:04:59');
+INSERT INTO material_categories (id, name, created_at, updated_at) VALUES (6,'魚','2021-11-03 02:04:59','2021-11-03 02:04:59');
+INSERT INTO material_categories (id, name, created_at, updated_at) VALUES (7,'肉','2021-11-03 02:04:59','2021-11-03 02:04:59');
+INSERT INTO material_categories (id, name, created_at, updated_at) VALUES (8,'嗜好品','2021-11-03 02:04:59','2021-11-03 02:04:59');
+INSERT INTO material_categories (id, name, created_at, updated_at) VALUES (9,'甘味類','2021-11-03 02:04:59','2021-11-03 02:04:59');
+INSERT INTO material_categories (id, name, created_at, updated_at) VALUES (10,'乾物','2021-11-03 02:04:59','2021-11-03 02:04:59');
+INSERT INTO material_categories (id, name, created_at, updated_at) VALUES (11,'油脂・調味料','2021-11-03 02:04:59','2021-11-03 02:04:59');
+INSERT INTO material_categories (id, name, created_at, updated_at) VALUES (12,'乳類','2021-11-03 02:04:59','2021-11-03 02:04:59');
+INSERT INTO material_categories (id, name, created_at, updated_at) VALUES (13,'卵類・たんぱく質','2021-11-03 02:04:59','2021-11-03 02:04:59');
