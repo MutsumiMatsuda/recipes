7bfd612f0d70b6b8ce195a9f3d1c5a8b1f8c05eb
MutsumiMatsuda <matsuda_mutsumi@r3.dion.ne.jp>
2021-11-29 00:05:41 +0900

    dump修正

--- dump.sql
+++ dump.sql
@@ -1,20 +1,3 @@
--- MySQL dump 10.13  Distrib 5.7.26, for Linux (x86_64)
---
--- Host: localhost    Database: recipes
--- ------------------------------------------------------
--- Server version	5.7.26
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO,POSTGRESQL' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Dumping data for table "howtos"
---
-
-/*!40000 ALTER TABLE howtos DISABLE KEYS */;
 INSERT INTO howtos (id, name, created_at, updated_at) VALUES (1,'圧力鍋','2021-11-01 20:26:42','2021-11-10 18:17:30');
 INSERT INTO howtos (id, name, created_at, updated_at) VALUES (2,'オーブン','2021-11-01 20:26:42','2021-11-01 20:26:42');
 INSERT INTO howtos (id, name, created_at, updated_at) VALUES (3,'レンジ','2021-11-01 20:26:42','2021-11-01 20:26:42');
